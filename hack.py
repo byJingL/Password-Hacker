9eeaef438c28da4996fb916c9587984dbb04af79
Jing <jing.lu.cn@outlook.com>
2023-01-04 12:29:12 +0000

    stage2 completed

--- hack.py
+++ hack.py
@@ -1,17 +1,35 @@
 import socket
 from argparse import ArgumentParser
+import itertools
+
+CHAR = '0123456789abcdefghijklmnopqrstuvwxyz'
+
+
+# Custom generator
+def find_password():
+    for i in range(1, len(CHAR)+1):
+        pool = itertools.product(CHAR, repeat=i)
+        for item in pool:
+            yield ''.join(item)
+
 
 parser = ArgumentParser()
 parser.add_argument('host', type=str, help='The ip address of the sever you want to connect.')
 parser.add_argument('port', type=int, help='The port of the sever you want to connect.')
-parser.add_argument('message', type=str, help='The message you want to send to sever.')
 args = parser.parse_args()
 
 with socket.socket() as client_connection:
     address = (args.host, args.port)
     client_connection.connect(address)
-    message = args.message.encode()
-    client_connection.send(message)
-    response = client_connection.recv(1024)
-    response = response.decode()
-    print(response)
+
+    password_generator = find_password()
+
+    for password in password_generator:
+        message = password.encode()
+        client_connection.send(message)
+        response = client_connection.recv(1024)
+        response = response.decode()
+
+        if response == 'Connection success!':
+            print(password)
+            break
